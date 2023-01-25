f4f853aa282bb30d899a7be0f656cb2cfb82fb79
Claudio Zicarelli <maczicarelli@gmail.com>
2023-01-25 19:52:03 -0300

    Proyecto Final CV

---
+++ js/cv.js
@@ -0,0 +1,7 @@
+// Seteo texto final al apretar el botón
+
+document.getElementById("boton").addEventListener("click", function () {
+    document.getElementById("bonus").style.color = "blue";
+    document.getElementById("bonus").style.textAlign = "center";
+    document.getElementById("bonus").style.fontWeight = "bold";
+})
